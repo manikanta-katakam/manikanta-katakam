977cb48b68641d3e71604a33a954741d2b28aafc
2300032036 <2300032036@kluniversity.in>
2026-06-15 12:29:36 +0530

    Migrate all premium gsap-trial imports back to standard gsap (>=3.13) to prevent ScrollSmoother deprecated deployment block page

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module "gsap-trial/ScrollSmoother" {
-  export const ScrollSmoother: any;
-  export default ScrollSmoother;
-}
-
-declare module "gsap-trial/SplitText" {
-  export const SplitText: any;
-  export default SplitText;
-}
